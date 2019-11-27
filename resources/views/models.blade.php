adf8eb936cac253357a5adb563bf72245d4b8626
Notorious88 <voodo.2015@mail.ru>
2019-11-27 18:38:19 +0300

    models

--- resources/views/models.blade.php
+++ resources/views/models.blade.php
@@ -8,16 +8,16 @@
             </div>
             <div class="col-12 downBlock " id="upBlock">
                 @foreach($models as $model)
-                    <div class=" col-xl-4 col-md-4 col-sm-6 col-xs-12 cont_for_models">
+                    <div class=" col-xl-4 col-md-4 col-sm-6 col-xs-6 cont_for_models">
                         <a class="link_model" href="/public/showModel/{{$model->id}}">{{$model->name}}</a>
                         <div class="cont_for_model">
                             <a href="/public/showModel/{{$model->id}}">
                                 <img src="{{ asset('/storage/' . $model->main_photo) }}">
                                 <div class="info_model">
-                                    <p>Возраст/Age: {{$model->age}}</p>
-                                    <p>Рост/Height: {{$model->height}}</p>
-                                    <p>Вес/Weight: {{$model->weight}}</p>
-                                    <p>Грудь/Chest: {{$model->chest}}</p>
+                                    <p class="inf_about">Возраст / Age: {{$model->age}}</p>
+                                    <p class="inf_about">Рост / Height: {{$model->height}}</p>
+                                    <p class="inf_about"> Вес / Weight: {{$model->weight}}</p>
+                                    <p class="inf_about">Грудь / Chest: {{$model->chest}}</p>
                                 </div>
                             </a>
                         </div>
