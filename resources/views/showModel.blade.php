b3fdbc384c0f7df40c84138fbbc6440f482f10a4
Notorious88 <voodo.2015@mail.ru>
2019-12-02 19:11:26 +0300

    model

--- resources/views/showModel.blade.php
+++ resources/views/showModel.blade.php
@@ -4,93 +4,112 @@
     <div id="contForPhotoModel">
         <i class="fa fa-times-circle noneCloseImg closeImg" id="closeImg" aria-hidden="true"></i>
         <div class="noneCloseImg" id="backImg">
-        <i class="fa fa-chevron-circle-left"  aria-hidden="true"></i>
+            <i class="fa fa-chevron-circle-left" aria-hidden="true"></i>
         </div>
         <img id='imgWindow' src="">
         <div class="noneCloseImg" id="nextImg">
             <i class="fa fa-chevron-circle-right" aria-hidden="true"></i>
         </div>
-        </div>
-    <div class="container-fluid">
-        <h1 class="text-center">{{$model->name}}</h1>
-            <div class="col-md-7 col-sm-12">
-                <img class="img-fluid" src="{{ asset('/storage/' .  $model->main_photo )}}">
-            </div>
-            <div class="col-md-5 col-sm-12">
-                <div>
-                    <h4 class="col-md-6 col-xs-6">Возраст: </h4>
-                    <h4 class="col-md-6 col-xs-6">{{$model->age}}</h4>
-                </div>
-                <div>
-                    <h4 class="col-md-6 col-xs-6">Рост: </h4>
-                    <h4 class="col-md-6 col-xs-6">{{$model->height}}</h4>
-                </div>
-                <div>
-                    <h4 class="col-md-6 col-xs-6">Вес: </h4>
-                    <h4 class="col-md-6 col-xs-6">{{$model->weight}}</h4>
-                </div>
-                <div>
-                    <h4 class="col-md-6 col-xs-6">Параметры: </h4>
-                    <h4 class="col-md-6 col-xs-6">{{$model->parameters}}</h4>
-                </div>
-                <div>
-                    <h4 class="col-md-6 col-xs-6">Грудь: </h4>
-                    <h4 class="col-md-6 col-xs-6">{{$model->chest}}</h4>
-                </div>
-                <div>
-                    <h4 class="col-md-6 col-xs-6">Город: </h4>
-                    <h4 class="col-md-6 col-xs-6">{{$model->city}}</h4>
-                </div>
-                <div>
-                    <h4 class="col-md-6 col-xs-6">Страна: </h4>
-                    <h4 class="col-md-6 col-xs-6">{{$model->country}}</h4>
-                </div>
-                <div>
-                    <h4 class="col-md-6 col-xs-6">Количество подписчиков в Instagram: </h4>
-                    <h4 class="col-md-6 col-xs-6">{{$model->followers}}</h4>
-                </div>
-                <div>
-                    <h4 class="col-md-6 col-xs-12">Статус: </h4>
-                    <h4 class="col-md-6 col-xs-12">{{$model->achievements}}</h4>
-                </div>
-                <div>
-                    @if (isset($model->one_photo))
-                    <div class="col-md-4 col-sm-12">
-                        <img  class="img-fluid imgModel" src="{{ asset('/storage/' .  $model->one_photo )}}">
-                    </div>
-                    @endif
-                    @if (isset($model->two_photo))
-                    <div class="col-md-4 col-sm-12">
-                        <img class="img-fluid imgModel" src="{{ asset('/storage/' .  $model->two_photo )}}">
-                    </div>
-                        @endif
-                        @if (isset($model->three_photo))
-                    <div class="col-md-4 col-sm-12">
-                        <img class="img-fluid imgModel" src="{{ asset('/storage/' .  $model->three_photo )}}">
-                    </div>
-                        @endif
-                        @if (isset($model->four_photo))
-                    <div class="col-md-4 col-sm-12">
-                        <img class="img-fluid imgModel" src="{{ asset('/storage/' .  $model->four_photo )}}">
-                    </div>
-                        @endif
-                        @if (isset($model->five_photo))
-                    <div class="col-md-4 col-sm-12">
-                        <img class="img-fluid imgModel" src="{{ asset('/storage/' .  $model->five_photo )}}">
-                    </div>
-                        @endif
-                        @if (isset($model->six_photo))
-                    <div class="col-md-4 col-sm-12">
-                        <img class="img-fluid imgModel" src="{{ asset('/storage/' .  $model->six_photo )}}">
-                    </div>
-                        @endif
-                </div>
+    </div>
 
-                <div>
-                    <a href="https://wa.me/79190477597?text=%D0%A1%D0%B0%D1%88%D0%B0+%D0%BB%D0%BE%D1%85">Оставить заявку</a> <p></p>
-                    <a href="https://t.me/joinchat/AAAAAEa8PaCFFmc9f4EWrA">Telegram</a>
+
+    <div class="container-fluid">
+        <div class="row for_centr">
+            <div class="col-12 nameModel">
+                <h1>{{$model->name}}</h1>
+            </div>
+            <div class="col-12">
+                <div class="col-md-6">
+                    <div class="mainPicture">
+                    <img class="img-fluid" src="{{ asset('/storage/' .  $model->main_photo )}}">
+                    </div>
+                </div>
+                <div class="col-md-6">
+                    <div>
+                        <div class="descriptionModel">
+                            <div>
+                                <p>Возраст / Age </p>
+                                <p>{{$model->age}}</p>
+                            </div>
+                            <div>
+                                <p>Рост / Height </p>
+                                <p>{{$model->height}}</p>
+                            </div>
+                            <div>
+                                <p>Вес / Weight </p>
+                                <p>{{$model->weight}}</p>
+                            </div>
+                            <div>
+                                <p>Параметры / Options </p>
+                                <p>{{$model->parameters}}</p>
+                            </div>
+                            <div>
+                                <p>Грудь / Chest </p>
+                                <p>{{$model->chest}}</p>
+                            </div>
+                            <div>
+                                <p>Город / City </p>
+                                <p>{{$model->city}}</p>
+                            </div>
+                            <div>
+                                <p>Страна / Country </p>
+                                <p>{{$model->country}}</p>
+                            </div>
+                            <div>
+                                <p>Подписчиков в Instagram <br/>
+                                    Instagram Followers</p>
+                                <p>{{$model->followers}}</p>
+                            </div>
+                            <div class="states">
+                                <h4 class="state">Статус / Status</h4>
+                                <h4>{{$model->achievements}}</h4>
+                            </div>
+                        </div>
+
+
+                        <div class="collectionPictures col-md-8">
+                            @if (isset($model->one_photo))
+                                <div class="col-xl-6 col-md-6 col-sm-12 ">
+                                    <img class="img-fluid imgModel" src="{{ asset('/storage/' .  $model->one_photo )}}">
+                                </div>
+                            @endif
+                            @if (isset($model->two_photo))
+                                <div class=" col-xl-6 col-md-6 col-sm-12">
+                                    <img class="img-fluid imgModel" src="{{ asset('/storage/' .  $model->two_photo )}}">
+                                </div>
+                            @endif
+                            @if (isset($model->three_photo))
+                                <div class="col-xl-6 col-md-6 col-sm-12">
+                                    <img class="img-fluid imgModel"
+                                         src="{{ asset('/storage/' .  $model->three_photo )}}">
+                                </div>
+                            @endif
+                            @if (isset($model->four_photo))
+                                <div class=" col-xl-6 col-md-6 col-sm-12">
+                                    <img class="img-fluid imgModel"
+                                         src="{{ asset('/storage/' .  $model->four_photo )}}">
+                                </div>
+                            @endif
+                            @if (isset($model->five_photo))
+                                <div class="col-xl-6 col-md-6 col-sm-12">
+                                    <img class="img-fluid imgModel"
+                                         src="{{ asset('/storage/' .  $model->five_photo )}}">
+                                </div>
+                            @endif
+                            @if (isset($model->six_photo))
+                                <div class="col-xl-6 col-md-6 col-sm-12">
+                                    <img class="img-fluid imgModel" src="{{ asset('/storage/' .  $model->six_photo )}}">
+                                </div>
+                            @endif
+                        </div>
+                    </div>
                 </div>
             </div>
+            <div class="col-md-12 request">
+                <a href="https://wa.me/79190477597?text=%D0%A1%D0%B0%D1%88%D0%B0+%D0%BB%D0%BE%D1%85">Оставить
+                    заявку</a>
+            </div>
+        </div>
     </div>
     <script src="/public/js/onlyModel.js"></script>
 @endsection
